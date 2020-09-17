832e0f3b9901134c08bdc7a99d9fe9650e543f49
Yagiz ALP <yagizwasmeow@gmail.com>
2020-09-17 17:59:10 +0300

    Update import.sql

--- import.sql
+++ import.sql
@@ -4,3 +4,11 @@ INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 ('cokekey', 'Key B', 1),
 ('methbag', 'Methbag (2G)', 100),
 ('cokebag', 'Cokebag (2G)', 100);
+
+CREATE TABLE `storages` (
+	`identifier` varchar(255) DEFAULT NULL,
+	`methstorage` varchar(255) DEFAULT NULL,
+	`cokestorage` varchar(255) DEFAULT NULL,
+
+	PRIMARY KEY (`identifier`)
+);

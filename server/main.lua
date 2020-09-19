dbdce375887817929158c54c0e590287c00aee04
Arne Van Daele (ItsDividerz) <arne.van.daele@icloud.com>
2020-09-19 05:51:46 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -29,12 +29,21 @@ end)
 RegisterNetEvent('esx_druglabs:server:clearCokeStorage')
 AddEventHandler('esx_druglabs:server:clearCokeStorage', function(amount)
     local sourcePlayer = ESX.GetPlayerFromId(source)
-
-    if sourcePlayer.canCarryItem('cokebag', amount) then
-        sourcePlayer.addInventoryItem('cokebag', amount)
-        sourcePlayer.showNotification("You cleared up the storage and got ~y~" .. amount .. "~w~ bags of cocaine.")
+    
+    if Config.usingWeight then
+        if sourcePlayer.canCarryItem('cokebag', amount) then
+            sourcePlayer.addInventoryItem('cokebag', amount)
+            sourcePlayer.showNotification("You cleared up the storage and got ~y~" .. amount .. "~w~ bags of cocaine.")
+        else
+            sourcePlayer.showNotification("You can't carry this amount of bags...")
+        end
     else
-        sourcePlayer.showNotification("You can't carry this amount of bags...")
+        local sourceItem = xPlayer.getInventoryItem('cokebag')
+        if sourceItem.limit ~= -1 and (sourceItem.count + amount) > sourceItem.limit then
+            sourcePlayer.showNotification("You can't carry any more cokebags...")
+        else
+            sourcePlayer.addInventoryItem('cokebag', amount)
+        end
     end
 end)
 
@@ -42,10 +51,19 @@ RegisterNetEvent('esx_druglabs:server:clearMethStorage')
 AddEventHandler('esx_druglabs:server:clearMethStorage', function(amount)
     local sourcePlayer = ESX.GetPlayerFromId(source)
 
-    if sourcePlayer.canCarryItem('methbag', amount) then
-        sourcePlayer.addInventoryItem('methbag', amount)
-        sourcePlayer.showNotification("You cleared up the storage and got ~y~" .. amount .. "~w~ bags of meth.")
+    if Config.usingWeight then
+        if sourcePlayer.canCarryItem('methbag', amount) then
+            sourcePlayer.addInventoryItem('methbag', amount)
+            sourcePlayer.showNotification("You cleared up the storage and got ~y~" .. amount .. "~w~ bags of meth.")
+        else
+            sourcePlayer.showNotification("You can't carry this amount of bags...")
+        end
     else
-        sourcePlayer.showNotification("You can't carry this amount of bags...")
+        local sourceItem = xPlayer.getInventoryItem('methbag')
+        if sourceItem.limit ~= -1 and (sourceItem.count + amount) > sourceItem.limit then
+            sourcePlayer.showNotification("You can't carry any more cokebags...")
+        else
+            sourcePlayer.addInventoryItem('methbag', amount)
+        end  
     end
 end)
